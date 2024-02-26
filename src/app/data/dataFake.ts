b3345278e5bb23b6bd93c5fe35bd1eea105d3ee7
Pedro Zeferino da Silva <zefpedro@hotmail.com>
2024-02-26 16:09:25 -0300

    Adicionando filmes na pasta data do projeto

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,15 +2,27 @@ export const dataFake = [
 
   {
     "id":"1",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "marvel anuncia um novo filme do homem de ferro, confira",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
   },
 
   {
     "id":"2",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "Nova Série anunciada no Disney +",
+    "description": "Liga da Justiça traz a equipe de vilões lutando contra os icônicos super-heróis da DC",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id": "3",
+    "title": "Marvel anuncia série de Pantera Negra para 2024 no Disney+",
+    "description": "A Marvel Studios anunciou oficialmente uma nova série animada no universo de Pantera Negra, que será lançada no Disney+. A notícia aparentemente é a confirmação de um rumor que circula desde 2021 e voltou a aparecer em setembro deste ano, quando o estúdio registrou alguns nomes para um projeto animado que estavam associados a uma iniciativa anterior do diretor Ryan Coogler.",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2023/12/Pantera-Negra-Disney-Plus.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Esquadrão Suicida: Mate a Liga da Justiça",
+    "description": "Esquadrão Suicida: Mate a Liga da Justiça traz a equipe de vilões lutando contra os icônicos super-heróis da DC",
+    "photoCover":"https://www.digitaltrends.com/wp-content/uploads/2023/11/Suicide-Squad-Kill-the-justice-League-key-art.jpg?fit=720%2C405&p=1"
   }
 ]
